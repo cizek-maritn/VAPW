15a1632e8c038ac8af8fdab1e252a453b2ae814f
Martin Čížek <officialpatera6@gmail.com>
2023-03-21 13:09:37 +0100

    Add files via upload

---
+++ vapw_cv4/vapw_cv4/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vapw_cv4
+{
+    public partial class Form1 : Form
+    {
+        int n = 1;
+        btn_cls[] arr = new btn_cls[20];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (n<20)
+            {
+                arr[n - 1] = new btn_cls(n);
+                this.Controls.Add(arr[n-1].getBtn());
+                n++;
+            }
+        }
+    }
+}
---
+++ vapw_cv4/vapw_cv4/btn_cls.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vapw_cv4
+{
+    internal class btn_cls
+    {
+        int number;
+        Button btn;
+        public btn_cls(int n)
+        {
+            this.number = n;
+            this.btn = new System.Windows.Forms.Button();
+
+            this.btn.Location = new System.Drawing.Point(301, 13+(25*n));
+            this.btn.Name = "btn" + n;
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = ""+n;
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+        }
+
+        public int getNum()
+        {
+            return this.number;
+        }
+
+        public Button getBtn()
+        {
+            return this.btn;
+        }
+
+        public void btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button number: " + this.number);
+        }
+    }
+}
